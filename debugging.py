767eb0fd701a69f6e8f1968d424e80a12d026dd9
ThomasBermudez <tbermudezm@ucenfotec.ac.cr>
2022-10-06 15:10:46 -0600

    Added try, except and raise to code

--- debugging.py
+++ debugging.py
@@ -1,15 +1,25 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num +1):
-        if num % i == 0:
-        #if num % i == 1: <---- Bug!
-            divisors.append(i)
-    return divisors
+    try:
+        if(num < 0):
+            raise ValueError('No se puede ingresar un numero negativo')
+        divisors = []
+        for i in range(1, num +1):
+            if num % i == 0:
+            #if num % i == 1: <---- Bug!
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Final")
+    try:
+        num = int(input("Ingresa un número: "))    
+        print(divisors(num))
+        print("Final")
+    except ValueError:
+        print("Se debe ingresar un numero")
+        
+        
 
 
 if __name__ == '__main__':
