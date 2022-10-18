996242d8c9cd2cbaf29cc9e6aae0fef7afd408dc
Thomas Bermudez Mora (saturnxs) <tbermudezm@ucenfotec.ac.cr>
2022-10-18 15:55:13 -0600

    Formatted code and updated comments

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -1,6 +1,7 @@
 import os, random
 
-# Have to create points system and hangman image
+# Have to create points system
+
 
 def menu():
     """
@@ -10,15 +11,15 @@ def menu():
     leaving = False
     errorMessage = ""
     option = 0
-    
+
     while not leaving:
         os.system('cls')
-          
+
         print("\n▄▀█ █░█ █▀█ █▀█ █▀▀ ▄▀█ █▀▄ █▀█\n█▀█ █▀█ █▄█ █▀▄ █▄▄ █▀█ █▄▀ █▄█\n")
         print("1. Nueva partida")
         print("2. Mi puntaje")
         print("3. Salir")
-        
+
         print("\n"+errorMessage)
         try:
             option = int(input("Ingresa una opción: "))
@@ -39,105 +40,99 @@ def menu():
 
 def sortWord():
     """
-    It opens a file, reads the contents, and returns a random word from the file
-    :return: The word selected
+    It's opening a file, reading the contents of the file, and then selecting a random word from the
+    file
+    :return: the wordSelected variable.
     """
     names = []
     wordSelected = ""
-    
+    toNormalize = (("á", "a"),("é", "e"),("í", "i"),("ó", "o"),("ú", "u"))
+
+    # It's opening a file, reading the contents of the file, and then selecting a random word from the
+    # file.
     with open("./files/data.txt", "r", encoding="utf-8") as file:
         for name in file:
             names.append(name.strip())
     wordSelected = names[random.randint(0, len(names))]
-    
-    # Replacing the accented vowels with their non-accented counterparts.
-    wordSelected = wordSelected.replace("á", "a")
-    wordSelected = wordSelected.replace("é", "e")
-    wordSelected = wordSelected.replace("í", "i")
-    wordSelected = wordSelected.replace("ó", "o")
-    wordSelected = wordSelected.replace("ú", "u")
-    
+
+    # It's replacing the accented vowels with their non-accented counterparts.
+    for accentedLetter, nonAccentedLetter in toNormalize:
+        wordSelected = wordSelected.replace(accentedLetter, nonAccentedLetter)
     return wordSelected
 
-        
-def game():
-    """
-    It creates a list of underscores that is the same length as the word that the user is trying to
-    guess, and then it asks the user to input a letter. If the letter is in the word, it updates the
-    list of underscores. If it isn't, it prints a message
-    """
+
+def game():  # Calculate difficulty based on word length
     hangmanImages = [
-        "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========", 
+        "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
-        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========", 
+        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
         "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="
-    ] 
-        
+    ]
+    failedChances = 0
+    lost = False
+    
     # Creating a list of underscores that is the same length as the word that the user is trying to guess.
     selectedWord = sortWord()
     hiddenWord = [("_") for letter in selectedWord]
+
     wordsNotFound = len(selectedWord)
-    
-    
-    # Creating a list of found letters and a message to be displayed to the user.
     foundLetters = []
     gameMessage = ""
-    failedChances = 0
-    notLost = True
-    
-    while wordsNotFound > 0 and notLost:
-        # The game loop. It prints the game board, asks the user to input a letter, and then checks if
-        # the letter is in the word. If it is, it updates the hidden word. If it isn't, it prints a
-        # message.
-        
+
+
+    # It's a loop that is running while the user hasn't lost and there are still letters to be found.
+    while wordsNotFound > 0 and not lost:
         os.system('cls')
         print("\n▄▀█ █▀▄ █ █░█ █ █▄░█ ▄▀█   █░░ ▄▀█   █▀█ ▄▀█ █░░ ▄▀█ █▄▄ █▀█ ▄▀█\n█▀█ █▄▀ █ ▀▄▀ █ █░▀█ █▀█   █▄▄ █▀█   █▀▀ █▀█ █▄▄ █▀█ █▄█ █▀▄ █▀█\n")
         print(hangmanImages[failedChances])
         print("\n"+" ".join(hiddenWord))
-        
+
         print("\n"+gameMessage)
-        try:  
+        try:
             # It's asking the user to input a letter, and then it's checking if the input is a letter.
             # If it isn't, it raises a ValueError.
             letter = input("Ingresa una letra: ").lower()
             if (len(letter) != 1 or letter.isdigit()):
                 raise ValueError('Ingresa solamente una letra')
-            
-            if(letterWasNotFound(foundLetters, letter)):
+
+            # It's checking if the letter that the user guessed is not in the list of found letters. If it isn't,
+            # it's checking if the letter is in the word. If it is, it's updating the hidden word, and
+            # adding the letter to the list of found letters. If it isn't, it's adding one to the failed chances, and checking
+            # if the user has lost.
+            if (letterWasNotFound(foundLetters, letter)):
                 foundLetterIndexes = getLettersIndexes(selectedWord, letter)
-                if(len(foundLetterIndexes) > 0):
+                if (len(foundLetterIndexes) > 0):
                     wordsNotFound = wordsNotFound - len(foundLetterIndexes)
                     updateHiddenWord(foundLetterIndexes, hiddenWord, letter)
                     foundLetters.append(letter)
                 else:
                     failedChances = failedChances + 1
-                    notLost = True if failedChances < 6 else False
-                
+                    lost = False if failedChances < 6 else True
+
                 gameMessage = ""
             else:
                 gameMessage = "Ya encontraste esta letra"
         except ValueError as ve:
             gameMessage = str(ve)
-            
 
     else:
         # A loop that asks the user if they want to play again or go back to the menu.
         alertMessage = ""
         option = 0
-        
+
         while True:
             os.system('cls')
             updateHiddenWord(foundLetterIndexes, hiddenWord, letter)
 
-            print("\n█▀▀ ▄▀█ █▄░█ ▄▀█ █▀ ▀█▀ █▀▀\n█▄█ █▀█ █░▀█ █▀█ ▄█ ░█░ ██▄\n") if notLost else print("\n█▀█ █▀▀ █▀█ █▀▄ █ █▀ ▀█▀ █▀▀\n█▀▀ ██▄ █▀▄ █▄▀ █ ▄█ ░█░ ██▄\n")
+            print("\n█▀▀ ▄▀█ █▄░█ ▄▀█ █▀ ▀█▀ █▀▀\n█▄█ █▀█ █░▀█ █▀█ ▄█ ░█░ ██▄\n") if not lost else print("\n█▀█ █▀▀ █▀█ █▀▄ █ █▀ ▀█▀ █▀▀\n█▀▀ ██▄ █▀▄ █▄▀ █ ▄█ ░█░ ██▄\n")
             print(hangmanImages[failedChances])
-            print("\nLa palabra era  "+" ".join(hiddenWord)+"\n") if notLost else print("\nLa palabra era  "+" ".join(selectedWord).upper()+"\n")
+            print("\nLa palabra era  "+" ".join(hiddenWord)+"\n") if not lost else print("\nLa palabra era  "+" ".join(selectedWord).upper()+"\n")
             print("1. Jugar de nuevo")
             print("2. Ir al menu")
-            
+
             print("\n"+alertMessage)
             try:
                 option = int(input("Ingresa una opción: "))
@@ -149,29 +144,29 @@ def game():
                     game()
                 case 2:
                     break
-    
-    
+
+
 def getLettersIndexes(word, letter):
     """
     It takes a word and a letter as input, and returns a list of indexes where the letter is found in
     the word
-    
+
     :param word: The word that the user is trying to guess
     :param letter: The letter that the user guessed
     :return: A list of indexes where the letter is found in the word.
     """
     matchingLettersIndex = []
     for index, character in enumerate(word):
-        if(character == letter):
+        if (character == letter):
             matchingLettersIndex.append(int(index))
     return matchingLettersIndex
-    
-    
+
+
 def updateHiddenWord(foundLetterIndexes, hiddenWord, letter):
     """
     It takes a list of indexes, a list of letters, and a letter, and returns a list of letters with the
     letter at the indexes replaced by the letter
-    
+
     :param foundLetterIndexes: a list of indexes where the letter was found in the word
     :param hiddenWord: The word that the user is trying to guess
     :param letter: The letter that the user guessed
@@ -185,20 +180,20 @@ def updateHiddenWord(foundLetterIndexes, hiddenWord, letter):
 def letterWasNotFound(foundLetters, newLetter):
     """
     If the new letter is not in the list of found letters, return True. Otherwise, return False
-    
+
     :param foundLetters: A list of letters that have been found so far
     :param newLetter: The letter that the user guessed
     :return: a boolean value.
     """
     for letter in foundLetters:
-        if(letter == newLetter):
+        if (letter == newLetter):
             return False
     return True
 
-        
+
 def run():
-    menu()  
-    
+    menu()
+
 
 if __name__ == "__main__":
     run()
