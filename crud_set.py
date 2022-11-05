dca3a04a18cf3db9004e11be27041b3b6e88822b
Thomas Bermudez Mora (saturnxs) <tbermudezm@ucenfotec.ac.cr>
2022-11-05 01:32:37 -0600

    Added comments

--- crud_set.py
+++ crud_set.py
@@ -10,6 +10,7 @@ def run():
 
     # ADD
 
+    # Adding the country Peru to the set.
     set_countries.add('pe')
     print('Add set_countries',set_countries)
     print('pe' in set_countries)
@@ -17,23 +18,27 @@ def run():
 
     # UPDATE
 
+    # Adding the elements in the set to the set_countries set.
     set_countries.update({'ar', 'ecua', 'pe'})
     print('Update set_countries',set_countries)
     
     
     # REMOVE
     
+    # Removing the element 'col' from the set.
     set_countries.remove('col')
     print('Remove set_countries',set_countries)
     
-    # DISCRD
+    # DISCARD
     
+    # Removing the element 'arg' from the set.
     set_countries.discard('arg')
     print('Discard set_countries', set_countries)
     
     
     # CLEAR
     
+    # Clearing the set.
     set_countries.clear()
     print('Set clear() len', len(set_countries))
     
